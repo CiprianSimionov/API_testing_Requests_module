bec4c5b65315b54c47717232cd046b66cdf52687
ciprian simionov <simionov.ciprian@gmail.com>
2025-06-05 16:05:17 +0300

    updated token request with Faker python package

--- requests_folder/generate_token_request.py
+++ requests_folder/generate_token_request.py
@@ -1,13 +1,22 @@
-from random import randint
-
 import requests
+from faker import Faker
 
 
-def generate_token():
-    random_nr = randint(1, 999999999)
-    data = {
-        "clientName": "Ciprian",
-        "clientEmail": f"ciprian.simionov{random_nr}@example.com"
+def generate_token(name="", email=""):
+    faker_object = Faker()
+    if name == "" and email != "":
+        name = faker_object.name()
+    elif name != "" and email == "":
+        email = faker_object.email()
+    elif name == "" and email == "":
+        name = faker_object.name()
+        email = faker_object.email()
+    else:
+        name = name
+        email = email
+    faker_data = {
+        "clientName": name,
+        "clientEmail": email
     }
-    response = requests.post("https://simple-books-api.glitch.me/api-clients/", json=data)
+    response = requests.post("https://simple-books-api.glitch.me/api-clients/", json=faker_data)
     return response.json()["accessToken"]
